dc08892ee1f356e393651f690c2611f147558828
Anmol Arora <heymailme04@gmail.com>
2024-03-30 17:19:18 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black-100': '#121212',
+        'gold-100':'#FAA432',
+        'darkGreen':'#0D9B4D'
+      },
+    },
   },
   plugins: [],
 }
