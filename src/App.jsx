b938c9332d8d6c4c16616da0b7e7bb97221317e2
BB9Kairo <85286527+calebjubal@users.noreply.github.com>
2024-04-11 11:44:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
 import BlogPage from "./pages/blogpage";
+import useFetch from "./hooks/useFetch";
 
 const App = () => {
+  let {loading, data, error} =useFetch('http://localhost:1337/api/blogs?populate=*')
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error!</p>
+  console.log(data)
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
