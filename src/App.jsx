242d5dfa59a40eef91b5ad22d41a1b30f06d1b98
Anmol Arora <heymailme04@gmail.com>
2024-04-16 19:33:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
 import BlogPage from "./pages/blogpage";
+import BlogPost from "./pages/blogpost";
+import ShopPage from "./pages/shoppage";
 import useFetch from "./hooks/useFetch";
+import NotFoundPage from "./pages/404";
 
 const App = () => {
-  let {loading, data, error} =useFetch('http://localhost:1337/api/blogs?populate=*')
-  if(loading) return <p>Loading...</p>
-  if(error) return <p>Error!</p>
-  console.log(data)
+  const { loading: blogLoading, data: blogData, error: blogError } = useFetch(
+    "http://localhost:1337/api/blogs?populate=*"
+  );
+  const { loading: shopLoading, data: shopData, error: shopError } = useFetch(
+    "http://localhost:1337/api/shops?populate=*"
+  );
+
+
+  if (blogLoading || shopLoading) return <p>Loading...</p>;
+  if (blogError || shopError) return <NotFoundPage />;
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/blog" element={<BlogPage blogs={data?data:""} />} />
+      <Route path="/blog" element={<BlogPage blogs={blogData ? blogData : []} />} />
+      <Route path="/blog/:id" element={<BlogPost blogs={blogData ? blogData : []} />} />
+      <Route path="/shop" element={<ShopPage shops={shopData ? shopData : []} />} />
     </Routes>
   );
 };
