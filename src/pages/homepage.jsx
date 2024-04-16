cefb009995bc8b5950298d76b42dd71c4793f64c
Anmol Arora <heymailme04@gmail.com>
2024-04-16 19:35:17 +0530

    Update homepage.jsx

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,11 +1,13 @@
 import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Link } from "react-router-dom";
 import Navbar from "../components/navbar";
 import Section from "../components/section";
 import Feature from "../components/feature";
-// import Product from "../components/product";
+import Product from "../components/product";
 import Pricing from "../components/pricing";
+import Footer from "../components/footer";
 import banner from "../assets/banner_round_bg.png";
 import product from "../assets/p1.png";
 import leaf from "../assets/leaf.png";
@@ -28,14 +30,14 @@ const HomePage = () => {
         { opacity: 0 },
         {
           opacity: 1,
-          duration: .5,
-          scale:1.1,
-          y:100,
+          duration: 0.5,
+          scale: 1.1,
+          y: 100,
           scrollTrigger: {
             trigger: ref.current,
-            start: 'top 10%', 
-            end: 'top 40%',
-            scrub:3,
+            start: "top 10%",
+            end: "top 40%",
+            scrub: 3,
           },
         }
       );
@@ -57,14 +59,17 @@ const HomePage = () => {
         },
       }
     );
+    // Calculate x position for itemRef2 (moving backward)
+    const xPosition2 = window.innerWidth * 0.1 - window.innerWidth * 0.05 + 240; // 50% of the screen width minus 20% plus 140 pixels
+
     gsap.fromTo(
       itemRef2.current,
       { opacity: 0.75, scale: 0.75 },
       {
         opacity: 1,
         scale: 0.5,
-        x: 140,
-        y: 50,
+        x: -xPosition2, // Move backward by the calculated distance
+        yPercent: 10, // 10% of the screen height
         rotateZ: -10,
         duration: 0.5,
         scrollTrigger: {
@@ -75,14 +80,18 @@ const HomePage = () => {
         },
       }
     );
+
+    // Calculate x position for itemRef3 (moving forward)
+    const xPosition3 = window.innerWidth * 0.1 - window.innerWidth * 0.05 + 240; // 50% of the screen width minus 20% plus 140 pixels
+
     gsap.fromTo(
       itemRef3.current,
       { opacity: 0.75, scale: 0.75 },
       {
         opacity: 1,
         scale: 0.5,
-        x: -400,
-        y: 50,
+        x: xPosition3, // Move forward by the same calculated distance
+        yPercent: 10, // 10% of the screen height
         rotateZ: 10,
         duration: 0.5,
         scrollTrigger: {
@@ -93,61 +102,62 @@ const HomePage = () => {
         },
       }
     );
+
     gsap.fromTo(
       imageRef.current,
       { scale: 0.75, y: -150 },
       { duration: 0.8, scale: 1, y: 0 }
     );
-
   }, []);
 
   return (
     <>
       <Navbar />
-      <div className="relative flex flex-col justify-center items-center h-screen mt-4 p-6 ">
-      <div className="text-center mb-12">
-  <img
-    ref={sideImgRefs[0]}
-    src={leaf}
-    alt=""
-    className="w-44 absolute h-44 rotate-45 left-20 top-10"
-  />
-  <img
-    ref={sideImgRefs[1]}
-    src={leaf}
-    alt=""
-    className="w-44 absolute h-44 -rotate-45 right-14 bottom-20"
-  />
-  <img
-    ref={sideImgRefs[2]}
-    src={leaf}
-    alt=""
-    className="w-44 absolute h-44 -rotate-12 left-20 bottom-60"
-  />
-  <img
-    ref={sideImgRefs[3]}
-    src={leaf}
-    alt=""
-    className="w-44 absolute h-44 -rotate-12 right-14 top-40"
-  />
-  <p className="text-sm font-bold lowercase text-gold-100 lg:text-xl">
-    #Harmonize Your Being
-  </p>
-  <h1 className="mb-4 mt-4 text-xl font-semibold text-slate-500 md:text-2xl lg:text-3xl uppercase">
-    <span className="text-transparent bg-clip-text bg-gradient-to-r lg:text-5xl  to-green-400 from-gold-100">
-      Ayurvedic Wellness Essentials..
-    </span>
-    <br />
-    the complete health companion
-  </h1>
-</div>
-
-        <button
-          type="button"
-          className="text-white border-2 border-white bg-gold-100 hover:bg-darkGreen focus:outline-none w-36 h-12 font-medium rounded-full text-sm px-5 py-2.5 -m-6 z-10  text-center transition duration-300 ease-in-out"
-        >
-          Explore Now
-        </button>
+      <div className="relative flex flex-col justify-center items-center h-screen md:mt-4 md:p-6 ">
+        <div className="text-center mb-4 md:mb-12">
+          <img
+            ref={sideImgRefs[0]}
+            src={leaf}
+            alt=""
+            className="w-44 hidden md:block absolute h-44 rotate-45 left-20 top-10"
+          />
+          <img
+            ref={sideImgRefs[1]}
+            src={leaf}
+            alt=""
+            className="w-44 hidden md:block absolute h-44 -rotate-45 right-14 bottom-20"
+          />
+          <img
+            ref={sideImgRefs[2]}
+            src={leaf}
+            alt=""
+            className="w-44 hidden md:block absolute h-44 -rotate-12 left-20 bottom-60"
+          />
+          <img
+            ref={sideImgRefs[3]}
+            src={leaf}
+            alt=""
+            className="w-44 hidden md:block absolute h-44 -rotate-12 right-14 top-40"
+          />
+          <p className="text-xl font-bold lowercase text-gold-100 md:text-xl">
+            #Harmonize Your Being
+          </p>
+          <h1 className="mb-4 mt-4 text-2xl font-semibold text-slate-500 md:text-2xl lg:text-3xl uppercase">
+            <span className="text-transparent  bg-clip-text bg-gradient-to-r lg:text-5xl to-green-400 from-gold-100 via-indigo-600">
+              Ayurvedic Wellness Essentials..
+            </span>
+            <br />
+            the complete health companion
+          </h1>
+        </div>
+        <Link to="/shop" className=" md:-m-6 z-10 mb-3 px-5 py-2.5">
+      <button
+        type="button"
+        className="text-white border-2 mb-3 border-white bg-gold-100 hover:bg-darkGreen focus:outline-none md:w-36 md:h-12 font-medium rounded-full text-sm px-5 py-2.5 md:-m-6 z-10 text-center transition duration-300 ease-in-out"
+      >
+        Explore Now
+      </button>
+    </Link>
         <img
           ref={imageRef}
           src={banner}
@@ -162,7 +172,7 @@ const HomePage = () => {
           title="SKU1"
           id="sku1"
           alt="P1"
-          className="absolute transform -translate-x-1/2 top-1/2 left-1/2 h-3/6 -mt-24 cursor-pointer"
+          className="absolute transform -translate-x-1/2 top-1/2 left-1/2 md:h-3/6 h-52 -mt-1 md:-mt-24 cursor-pointer"
         />
         <img
           ref={itemRef2}
@@ -170,7 +180,7 @@ const HomePage = () => {
           title="SKU2"
           id="sku2"
           alt="P2"
-          className="absolute transform -translate-x-1/2 top-1/2 left-1/2 h-3/6 -mt-24 cursor-pointer"
+          className="absolute transform -translate-x-1/2 top-1/2 left-1/2 md:h-3/6 hidden md:block md:-mt-24 cursor-pointer"
         />
         <img
           ref={itemRef3}
@@ -178,14 +188,15 @@ const HomePage = () => {
           title="SKU3"
           id="sku3"
           alt="P3"
-          className="absolute transform -translate-x-1/2 top-1/2 left-1/2 h-3/6 -mt-24 cursor-pointer"
+          className="absolute transform -translate-x-1/2 top-1/2 left-1/2 md:h-3/6 hidden md:block md:-mt-24 cursor-pointer"
         />
       </div>
 
       <Section />
-      {/* <Product /> */}
-      <Pricing/>
-      <Feature />  
+      <Pricing />
+      <Feature />
+      <Product />
+      <Footer />
     </>
   );
 };
