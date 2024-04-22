de195b952eb840bb0616007e89051339ee136540
Anmol Arora <heymailme04@gmail.com>
2024-04-22 12:36:34 +0530

    Update drawer.jsx

--- src/components/drawer.jsx
+++ src/components/drawer.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { RiMenu3Fill } from "react-icons/ri";
+import React, { useState, useEffect } from "react";
+import { RiShoppingCartLine } from "react-icons/ri";
 
 const Drawer = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -8,34 +8,38 @@ const Drawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
 
+  useEffect(() => {
+    if (isDrawerOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isDrawerOpen]);
+
   return (
     <>
-      <div className="text-center hidden md:block">
+      <div className="text-center">
         <button onClick={toggleDrawer} className="focus:outline-none">
-          <RiMenu3Fill
+          <RiShoppingCartLine
             size={32}
-            color=""
-            className="my-icon hover:cursor-pointer text-green-500 hover:scale-105 hover:text-green-700 transition duration-300 ease-in-out"
+            color="gray"
+            className="my-icon hover:cursor-pointer hover:scale-90 transition duration-300 ease-in-out"
           />
         </button>
       </div>
 
       <div
-        className={`fixed top-0 right-0 z-40 h-screen p-6 overflow-y-auto bg-gray-100 w-96 transform transition-transform duration-500 ${
+        className={`fixed top-0 right-0 z-40 h-screen p-6 overflow-y-auto bg-gray-100 w-96 transform transition-transform duration-300 ${
           isDrawerOpen ? "translate-x-0" : "translate-x-full scale-80 opacity-80"
         }`}
       >
         <div className="drawer-content">
-          {/* Drawer content */}
-          <h1 className="mb-4 mt-10 font-medium leading-none tracking-tight text-gray-900 md:text-2xl lg:text-4xl ">
-            Get back to health with{" "}
-            <span className="text-green-600 ">Blaze #1</span>
+          <h1 className="font-medium absolute top-2.5 start-3 flex items-center justify-center text-black-100 md:text-lg lg:text-xl ">
+            Shopping Cart
           </h1>
-          <p className="text-lg font-normal text-gray-500 lg:text-base text-justify ">
-            Here at DAC, we focus on markets where technology, innovation, and
-            capital can unlock long-term value and drive economic growth.
-          </p>
-
           <button
             type="button"
             onClick={toggleDrawer}
