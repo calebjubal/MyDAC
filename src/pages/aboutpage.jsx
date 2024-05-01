b747a6b0a989771de05696f0ab6c9ffac6b778ca
Dheeraj Kumar <devdheeraj83@gmail.com>
2024-05-02 01:28:21 +0530

    Create aboutpage.jsx
    About Us Page

---
+++ src/pages/aboutpage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Abtftr from "../components/aboutfeature";
+import Section from "../components/section";
+
+
+
+const AboutPage = ()=>{
+    
+}
+return(
+    <>
+    <Navbar/>
+    <Section/>
+    <Abtftr/>
+    
+    </>
+);
+
+export default AboutPage;
+
